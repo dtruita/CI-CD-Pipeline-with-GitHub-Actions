cf4668c42488f5818fbf99e770c4aa2c03d53f44
Domz <dtruita@gmail.com>
2025-06-06 13:42:10 +0800

    update ci.yml and db.php

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $dbname = "gdrhkwmq_test";
 $username = "gdrhkwmq_root";
-$password = "i65=K{(m8505";
+$password = "&E}[26V@EEfo";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
